02b41f1ade147f11841a67801da5b052a5c729f4
MauriceHumado <mauricehumado@gmail.com>
2022-11-22 21:22:15 -0800

    _printf.c

--- _printf.c
+++ _printf.c
@@ -6,6 +6,30 @@
 #include <limits.h>
 
 /**
+ * get_func - look for the specifier
+ * @x: variable to the function
+ * Return: function
+ */
+
+int (*get_func(char x))(va_list)
+{
+	int i = 0;
+	spec arr[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+	while (arr[i].valid)
+	{
+		if (x == arr[i].valid[0])
+			return (arr[i].f);
+		i++;
+	}
+	return (NULL);
+}
+
+/**
  * _printf - a function that produces output according to a format
  * @format: character string
  * Return: the number of characters printed
