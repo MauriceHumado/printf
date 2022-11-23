644b6dd688eb608f60acd251ce4c2143dbbd9905
MauriceHumado <mauricehumado@gmail.com>
2022-11-22 20:55:48 -0800

    main.h

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+int _printf(const char *format, ...);
+int (*get_func(char x))(va_list args);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_percent(va_list args);
+int _putchar(char c);
+
+/**
+ * struct specifier - struct specifier
+ * @valid: the valid character
+ * @f: the functions associated
+ */
+
+typedef struct specifier
+{
+	char *valid;
+	int (*f)(va_list);
+} spec;
+
+#endif
