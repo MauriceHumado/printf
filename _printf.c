266ee3c8ddf945583910d333fdcd23d81a65c97d
MauriceHumado <mauricehumado@gmail.com>
2022-11-25 05:37:49 -0800

    printf function

--- _printf.c
+++ _printf.c
@@ -6,115 +6,50 @@
 #include <limits.h>
 
 /**
- * get_func - look for the specifier
- * @x: variable to the function
- * Return: function
- */
-
-int (*get_func(char x))(va_list)
-{
-	int i = 0;
-	spec arr[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"%", print_percent},
-		{NULL, NULL}
-	};
-	while (arr[i].valid)
-	{
-		if (x == arr[i].valid[0])
-			return (arr[i].f);
-		i++;
-	}
-	return (NULL);
-}
-
-/**
- * _printf - a function that produces output according to a format
- * @format: character string
- * Return: the number of characters printed
+ * _printf - produces ouput according to a format
+ * @format: format string containint characters and specifiers
+ * Description: to call the get_print()function which will call
+ * a print funcion depending on conversion specifier contained in format
+ * Return: length of formatted output string
  */
 
 int _printf(const char *format, ...)
 {
-	if (format != NULL)
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
+
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		int count = 0, i;
-		int (*m)(va_list);
-		va_list args;
-
-		va_start(args, format);
-		i = 0;
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-		while (format != NULL && format[i] != '\0')
+		if (*p == '%')
 		{
-			if (format[i] == '%')
+			p++;
+			if (*p == '%')
 			{
-				if (format[i + 1] == '%')
-				{
-					count += putchar(format[i]);
-					i += 2;
-				}
-				else
-				{
-					m = get_func(format[i + 1]);
-					if (m)
-						count += m(args);
-					else
-						count = putchar(format[i]) + putchar(format[i + 1]);
-					i += 2;
-				}
+				count += _putchar('%');
+				continue;
 			}
-			else
-			{
-				count += putchar(format[i]);
-				i++;
-			}
+
+			while (get_flag(*p, &flags))
+				p++;
+
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
 		}
-		va_end(args);
-		return (count);
+		else
+			count += _putchar(*p);
 	}
-	return (-1);
-}
-
-/**
- * main - Entry point
- * Return: Always 0
- */
-
-int main(void)
-{
-	int len;
-	int len2;
-	unsigned int ui;
-	void *addr;
-
-	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
-	ui = (unsigned int)INT_MAX + 1024;
-	addr = (void *)0x7ffe637541f0;
-	_printf("Length:[%d, %i]\n", len, len);
-	printf("Length:[%d, %i]\n", len2, len2);
-	_printf("Negative:[%d]\n", -762534);
-	printf("Negative:[%d]\n", -762534);
-	_printf("Unsigned:[%u]\n", ui);
-	printf("Unsigned:[%u]\n", ui);
-	_printf("Unsigned octal:[%o]\n", ui);
-	printf("Unsigned octal:[%o]\n", ui);
-	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
-	printf("String:[%s]\n", "I am a string !");
-	_printf("Address:[%p]\n", addr);
-	printf("Address:[%p]\n", addr);
-	len = _printf("Percent:[%%]\n");
-	len2 = printf("Percent:[%%]\n");
-	_printf("Len:[%d]\n", len);
-	printf("Len:[%d]\n", len2);
-/*	_printf("Unknown:[%r]\n");*/
-/*	printf("Unknown:[%r]\n");*/
-	return (0);
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 }
